95be27d5fde4b4206e359a7d9bda99e55a102c5f
blankdean <blankdean@gmail.com>
2022-10-15 22:44:53 -0400

    fix automation script

--- start_vehicles.sh
+++ start_vehicles.sh
@@ -2,12 +2,6 @@
 
 read -p "How many vehicle client containers do you want to start? " count
 
-echo "Stopping and Removing all old containers..."
-
-sudo docker stop $(sudo docker ps -a -q)
-sudo docker rm $(sudo docker ps -a -q)
-
-
 echo "Starting $count Vehicle Client Containers..."
 
 for ((i=0; i<$count; i++))
