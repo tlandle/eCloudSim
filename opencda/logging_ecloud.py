e89726f1cfd7d01d5b996e499a16c69204935cbe
blankdean <blankdean@gmail.com>
2022-11-11 13:14:16 -0500

    create log directory

--- opencda/logging_ecloud.py
+++ opencda/logging_ecloud.py
@@ -1,11 +1,14 @@
 import logging
 from datetime import datetime
+import os
+
+if not os.path.exists('log'):
+   os.makedirs('log')
+
 LOG_FILENAME = datetime.now().strftime('log/logfile_%H_%M_%S_%d_%m_%Y.log')
 
 for handler in logging.root.handlers[:]:
     logging.root.removeHandler(handler)
-
-open(LOG_FILENAME,'a+') # creates log file if it doesn't exist
 
 logging.basicConfig(filename=LOG_FILENAME,level=logging.DEBUG)  
 
