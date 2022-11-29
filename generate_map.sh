d7870fca79c93f83c06bdea44566b838e3130eee
Tyler Landle <tyler.landle@gmail.com>
2022-11-29 14:32:07 -0500

    adding generation script to simplify

---
+++ generate_map.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+export CARLA_ROOT=~/carla-0.9.12
+export PYTHONPATH=$PYTHONPATH:$CARLA_ROOT/PythonAPI/carla
+export PYTHONPATH=$PYTHONPATH:$CARLA_ROOT/PythonAPI/carla/dist/carla-0.9.12-py3.7-linux-x86_64.egg
+export PYTHONPATH=$PYTHONPATH:leaderboard
+export PYTHONPATH=$PYTHONPATH:leaderboard/team_code
+export PYTHONPATH=$PYTHONPATH:scenario_runner
+
+python3 generate_map_waypoints.py
