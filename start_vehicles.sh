39901d5ff9d527fd382299902514173f68902321
Ubuntu <azureuser@carla-ndm.0ef4z0xin4pujmnwmxxgjvjmyb.bx.internal.cloudapp.net>
2023-08-03 20:55:28 +0000

    Updating dockerfile to work, other client comms fixes. Sim still not working

--- start_vehicles.sh
+++ start_vehicles.sh
@@ -22,7 +22,7 @@ do
         sudo nvidia-docker run --gpus all -d --network=host -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$DISPLAY vehicle-sim --apply_ml 
         # sudo docker run -d --network=host vehicle-sim
     else 
-        sudo nvidia-docker run --rm --gpus all -d --network=host -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$DISPLAY vehicle-sim
+        sudo nvidia-docker run --gpus all -d --network=host -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$DISPLAY vehicle-sim
     fi    
 done
 
