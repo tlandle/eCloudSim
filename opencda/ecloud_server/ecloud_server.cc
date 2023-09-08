e1a892f6308c8f900d026c92d3c298dd875c3815
Jordan Rapp <jordanrapp@gmail.com>
2023-09-07 20:04:03 -0700

    edge stuff

--- opencda/ecloud_server/ecloud_server.cc
+++ opencda/ecloud_server/ecloud_server.cc
@@ -14,6 +14,7 @@
 #include "absl/flags/flag.h"
 #include "absl/flags/parse.h"
 #include "absl/strings/str_format.h"
+#include "absl/log/log.h"
 
 #include <grpcpp/ext/proto_server_reflection_plugin.h>
 #include <grpcpp/grpcpp.h>
@@ -53,16 +54,17 @@ using ecloud::LocDebugHelper;
 using ecloud::AgentDebugHelper;
 using ecloud::PlanerDebugHelper;
 using ecloud::ClientDebugHelper;
+using ecloud::WaypointRequest;
+using ecloud::EdgeWaypoints;
 
+#define _DEBUG
+//#define NDEBUG
 #define WORLD_TICK_DEFAULT_MS 50
 
 static void _sig_handler(int signo) 
 {
     if (signo == SIGTERM || signo == SIGINT) 
     {
-        // server_interrupt_func();
-        // shutdown_func();
-        // google::ShutdownGoogleLogging();
         exit(signo);
     }
 }
@@ -82,6 +84,8 @@ std::string version_;
 
 State simState_;
 Command command_;
+
+std::vector<std::pair<int16_t, std::string>> serializedEdgeWaypoints_; // vehicleIdx, serializedWPBuffer
 
 absl::Mutex mu_;
 std::vector<std::string> pendingReplies_ ABSL_GUARDED_BY(mu_); // serialized protobuf
@@ -122,8 +126,8 @@ public:
         if ( logLevel_ > 0 && ( pong->tick_id() - 1 ) % 5 == 0 && new_ )
         {
             const auto now = std::chrono::system_clock::now();
-            std::cout << "LOG(DEBUG) " << "sent new tick " << pong->tick_id() << " at " << std::chrono::duration_cast<std::chrono::milliseconds>(
-                now.time_since_epoch()).count() << std::endl;
+            LOG(INFO) << "sent new tick " << pong->tick_id() << " at " << std::chrono::duration_cast<std::chrono::milliseconds>(
+                now.time_since_epoch()).count();
         }
 
         ServerUnaryReactor* reactor = context->DefaultReactor();
@@ -139,7 +143,7 @@ public:
         const bool complete_ = ( replies_ + completions_ ) == numCars_;
 
         if ( complete_ )
-            std::cout << "LOG(INFO) Server_Ping COMPLETE" << std::endl;
+            LOG(INFO) << "Server_Ping COMPLETE";
 
         if ( simState_ == State::NEW )
         {
@@ -154,8 +158,8 @@ public:
         if ( logLevel_ > 0 && complete_ )
         {
             const auto now = std::chrono::system_clock::now();
-            std::cout << "LOG(DEBUG) " << "tick " << tickId_ << " complete at " << std::chrono::duration_cast<std::chrono::milliseconds>(
-                now.time_since_epoch()).count() << std::endl;    
+            LOG(INFO) << "tick " << tickId_ << " complete at " << std::chrono::duration_cast<std::chrono::milliseconds>(
+                now.time_since_epoch()).count();    
         }
 
         ServerUnaryReactor* reactor = context->DefaultReactor();
@@ -196,16 +200,16 @@ public:
         if ( logLevel_ > 0 && ( tickId_ - 1 ) % 5 == 0 )
         {
             const auto now = std::chrono::system_clock::now();
-            std::cout << "LOG(DEBUG) " << "received OK from vehicle " << request->vehicle_index() << " taking " << request->step_time_ms() << "ms for tick " << tickId_ << " at " << std::chrono::duration_cast<std::chrono::milliseconds>(
-                now.time_since_epoch()).count() << std::endl;
+            LOG(INFO) << "received OK from vehicle " << request->vehicle_index() << " taking " << request->step_time_ms() << "ms for tick " << tickId_ << " at " << std::chrono::duration_cast<std::chrono::milliseconds>(
+                now.time_since_epoch()).count();
         }    
 
-        // std::cout << "LOG(DEBUG) " << "Client_SendUpdate - received reply from vehicle " << request->vehicle_index() << " for tick id:" << request->tick_id() << std::endl;
+        DLOG(INFO) << "Client_SendUpdate - received reply from vehicle " << request->vehicle_index() << " for tick id:" << request->tick_id();
 
         if ( request->vehicle_state() == VehicleState::TICK_DONE )
         {
             numCompletedVehicles_++;
-            // std::cout << "LOG(DEBUG) " << "Client_SendUpdate - TICK_DONE - tick id: " << tickId_ << " vehicle id: " << request->vehicle_index() << std::endl;
+            DLOG(INFO) << "Client_SendUpdate - TICK_DONE - tick id: " << tickId_ << " vehicle id: " << request->vehicle_index();
         }
         else if ( request->vehicle_state() == VehicleState::TICK_OK )
         {
@@ -214,13 +218,40 @@ public:
         else if ( request->vehicle_state() == VehicleState::DEBUG_INFO_UPDATE )
         {
             numCompletedVehicles_++;
-            // std::cout << "LOG(DEBUG) " << "Client_SendUpdate - DEBUG_INFO_UPDATE - tick id: " << tickId_ << " vehicle id: " << request->vehicle_index() << std::endl;
+            DLOG(INFO) << "Client_SendUpdate - DEBUG_INFO_UPDATE - tick id: " << tickId_ << " vehicle id: " << request->vehicle_index();
         }
 
-        // std::cout << "LOG(DEBUG) " << "Client_SendUpdate - replying tick id: " << tickId_ << std::endl;
+        DLOG(INFO) << "Client_SendUpdate - replying tick id: " << tickId_;
         reply->set_tick_id(tickId_.load());
         reply->set_last_world_tick_time_ms(lastWorldTickTimeMS_.load());
 
+        ServerUnaryReactor* reactor = context->DefaultReactor();
+        reactor->Finish(Status::OK);
+        return reactor;
+    }
+
+    // server can push WP *before* ticking world and client can fetch them before it ticks
+    ServerUnaryReactor* Client_GetWaypoints(CallbackServerContext* context,
+                               const WaypointRequest* request,
+                               WaypointBuffer* buffer) override {
+        
+        for ( int i = 0; i < serializedEdgeWaypoints_.size(); i++ )
+        {
+            const std::pair<int16_t, std::string > wpPair = serializedEdgeWaypoints_[i];
+            if ( wpPair.first == request->vehicle_index() )
+            {
+                buffer->set_vehicle_index(request->vehicle_index());
+                WaypointBuffer *wpBuf;
+                wpBuf->ParseFromString(wpPair.second);
+                for ( Waypoint wp : wpBuf->waypoint_buffer())
+                {
+                    Waypoint *p = buffer->add_waypoint_buffer();
+                    p->CopyFrom(wp);
+                }
+                break;
+            }
+        }
+        
         ServerUnaryReactor* reactor = context->DefaultReactor();
         reactor->Finish(Status::OK);
         return reactor;
@@ -234,7 +265,7 @@ public:
 
         if ( request->vehicle_state() == VehicleState::REGISTERING )
         {
-            //// std::cout << "LOG(DEBUG) " << "got a registration update" << std::endl;
+            DLOG(INFO) << "got a registration update";
 
             reply->set_state(State::NEW);
             reply->set_tick_id(0);
@@ -243,19 +274,17 @@ public:
             reply->set_application(application_);
             reply->set_version(version_);
             
-            // std::cout << "LOG(DEBUG) " << "RegisterVehicle - REGISTERING - vehicle id: " << reply->vehicle_index() << std::endl;
+            DLOG(INFO) << "RegisterVehicle - REGISTERING - vehicle id: " << reply->vehicle_index();
             
             numRegisteredVehicles_++;
         }
         else if ( request->vehicle_state() == VehicleState::CARLA_UPDATE )
-        {
-            // std::cout << "LOG(DEBUG) " << "got a carla update" << std::endl;
-            
+        {            
             reply->set_state(State::START); // # do we need a new state? like "registering"?
             reply->set_tick_id(0);
             reply->set_vehicle_index(request->vehicle_index());
             
-            // std::cout << "LOG(DEBUG) " << "RegisterVehicle - CARLA_UPDATE - vehicle_index: " << request->vehicle_index() << " | actor_id: " << request->actor_id() << " | vid: " << request->vid() << std::endl;
+            DLOG(INFO) << "RegisterVehicle - CARLA_UPDATE - vehicle_index: " << request->vehicle_index() << " | actor_id: " << request->actor_id() << " | vid: " << request->vid();
             
             mu_.Lock();
             std::string msg;
@@ -289,8 +318,25 @@ public:
         if (logLevel_ > 0)
         {
             const auto now = std::chrono::system_clock::now();
-            std::cout << "LOG(DEBUG) " << "received new tick " << request->tick_id() << " at " << std::chrono::duration_cast<std::chrono::milliseconds>(
-                now.time_since_epoch()).count() << std::endl;
+            LOG(INFO) << "received new tick " << request->tick_id() << " at " << std::chrono::duration_cast<std::chrono::milliseconds>(
+                now.time_since_epoch()).count();
+        }
+
+        ServerUnaryReactor* reactor = context->DefaultReactor();
+        reactor->Finish(Status::OK);
+        return reactor;
+    }
+
+    ServerUnaryReactor* Server_PushEdgeWaypoints(CallbackServerContext* context,
+                               const EdgeWaypoints* edgeWaypoints,
+                               Empty* empty) override {
+        serializedEdgeWaypoints_.clear();
+
+        for ( WaypointBuffer wpBuf : edgeWaypoints->all_waypoint_buffers() )
+        {   std::string serializedWPs;
+            wpBuf.SerializeToString(&serializedWPs);
+            const std::pair< int16_t, std::string > wpPair = std::make_pair( wpBuf.vehicle_index(), serializedWPs );
+            serializedEdgeWaypoints_.push_back(wpPair);
         }
 
         ServerUnaryReactor* reactor = context->DefaultReactor();
@@ -353,7 +399,7 @@ void RunServer(uint16_t port) {
         10 * 1000 /*10 sec*/);
     // Finally assemble the server.
     std::unique_ptr<Server> server(builder.BuildAndStart());
-    std::cout << "LOG(INFO) " << "Server listening on " << server_address << std::endl;
+    LOG(INFO) << "Server listening on " << server_address;
 
     // Wait for the server to shutdown. Note that some other thread must be
     // responsible for shutting down the server for this call to ever return.
@@ -372,15 +418,7 @@ int main(int argc, char** argv) {
             exit(EXIT_FAILURE);
     }
 
-    //int debug_level = static_cast<int>(google::INFO);
-
     absl::ParseCommandLine(argc, argv);
-
-    //FLAGS_alsologtostderr = debug_level == google::INFO ? true : false;
-    //FLAGS_minloglevel = debug_level;
-    //google::InitGoogleLogging(argv[0]);
-
-    logLevel_ = absl::GetFlag(FLAGS_log_level);
 
     std::thread vehicle_one_server = std::thread(&RunServer,absl::GetFlag(FLAGS_vehicle_one_port));
     std::thread vehicle_two_server = std::thread(&RunServer,absl::GetFlag(FLAGS_vehicle_two_port));
