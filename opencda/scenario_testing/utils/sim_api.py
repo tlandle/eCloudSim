33c5acd8eee0114cac3da6b5fb8ea8ace4f7ccb6
Jordan Rapp <jrappp@respawn.com>
2023-09-12 11:39:13 -0700

    nit

--- opencda/scenario_testing/utils/sim_api.py
+++ opencda/scenario_testing/utils/sim_api.py
@@ -328,7 +328,7 @@ class ScenarioManager:
                  distributed=False):
 
         self.run_distributed = distributed
-        if distributed and ECLOUD_IP == 'localhost':
+        if distributed and ECLOUD_IP == 'localhost' or ECLOUD_IP == CARLA_IP:
             server_log_level = 0 if logger.getEffectiveLevel() == logging.DEBUG else \
                                 1 if logger.getEffectiveLevel() == logging.WARNING else 2 # 1: WARNING | 2: ERROR
             try:
