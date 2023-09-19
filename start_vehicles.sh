fc940b27e9902018de11a029f0f1f5d688468bc7
Ubuntu <azureuser@carla-server.ylysqs31cfleripcsggip1s3of.cx.internal.cloudapp.net>
2023-09-19 03:27:24 +0000

    Fixing some bugs in here - gpus can't be assigned if there are none and you get divide by 0. Also need sudo for docker stop

--- start_vehicles.sh
+++ start_vehicles.sh
@@ -9,7 +9,7 @@ if [[ "$rebuild" = "Y" || "$rebuild" = "y" ]]; then
     sudo docker build -f Dockerfile -t vehicle-sim:latest .
 fi
 
-running_containers=$(docker ps -a -q | wc -l)
+running_containers=$(sudo docker ps -a -q | wc -l)
 if (( running_containers > 1 )); then
     echo "WARNING: you had old containers - prefer to run stop_vehicles before starting a new scenario." 
     echo "stopping and removing all old containers..."
@@ -20,24 +20,25 @@ fi
 
 echo "starting $count vehicle client vontainers..."
 
-gpu=0
-num_gpus=$(nvidia-smi -L | wc -l)
-echo "this machine has $num_gpus gpu cores"
 for ((i=0; i<$count; i++))
 do
     if [[ "$use_ml" = "Y" || "$use_ml" = "y" ]]; then
+        gpu=0
+        num_gpus=$(nvidia-smi -L | wc -l)
+        echo "this machine has $num_gpus gpu cores"
         echo "container $i pinned to gpu $gpu"
         sudo docker run --runtime=nvidia --gpus device=$gpu -d --network=host --name=container_$i -e "HOSTNAME=container_$i" -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$DISPLAY vehicle-sim --apply_ml
+        ((gpu++))
+        #echo "$gpu % $num_gpus = $(( gpu % num_gpus ))"
+        if (( $(( gpu % num_gpus )) == 0 )); then
+            echo "resetting gpu round robin"
+            gpu=0
+        fi
     else
         #sudo docker run --runtime=nvidia --gpus all -d --network=host --name=container_$i -e "HOSTNAME=container_$i" -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$DISPLAY vehicle-sim
         sudo docker run -d --network=host --name=container_$i -e "HOSTNAME=container_$i" vehicle-sim
     fi
-    ((gpu++))
-    #echo "$gpu % $num_gpus = $(( gpu % num_gpus ))"
-    if (( $(( gpu % num_gpus )) == 0 )); then
-        echo "resetting gpu round robin"
-        gpu=0
-    fi
-done
+    done
 
 sudo docker container ls
+
